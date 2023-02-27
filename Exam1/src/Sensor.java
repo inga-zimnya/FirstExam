fb3d15085834106126bfb9d069a9260d0ec3870e
Inga <engaezik@gmail.com>
2023-02-27 23:15:44 +0300

    First task and second

---
+++ Exam1/src/Sensor.java
@@ -0,0 +1,10 @@
+public class Sensor {
+    public static int humValue;
+    public static int getRandomDiceNumber()
+    {
+        humValue = (int) (Math.random() * 101);
+        return humValue;
+    }
+
+
+}
---
+++ Exam1/src/Task2.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Random;
+public class Task2 {
+    public static void main(String[] args) {
+        int year = 0, month = 0, day = 0;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите год полива кактуса: ");
+        try{
+            year = scan.nextInt();
+        }catch(InputMismatchException e){
+            throw new InputMismatchException("Неправильный формат строки! Введите число.");
+        }
+
+        if(year < 1 || year > 2023){
+            System.out.println("Невозможное значение года");
+        }
+
+        System.out.println("Введите месяц полива кактуса, если январь - 1, декабрь - 12: ");
+
+        try{
+            month = scan.nextInt();
+        }catch(InputMismatchException e){
+            throw new InputMismatchException("Неправильный формат строки! Введите число.");
+        }
+
+        month -= 1;
+
+        if(month < 0 || month > 11){
+            throw new InputMismatchException("Невозможное значение месяца");
+        }
+
+        System.out.println("Введите день полива кактуса: ");
+
+        try{
+            day = scan.nextInt();
+        }catch(InputMismatchException e){
+            throw new InputMismatchException("Неправильный формат строки! Введите число.");
+        }
+
+        boolean highYear = (year%4 == 0 && year%100 != 0 || year%400 == 0);
+
+        if(day < 1 || day > 31 || (month == 1 && highYear && day > 29)){
+            throw new InputMismatchException("Невозможное значение дня");
+        }
+        scan.close();
+
+        Calendar calendar = new GregorianCalendar(year, month , day);
+        Date date = calendar.getTime();
+        System.out.println(date);
+
+
+    }
+}
